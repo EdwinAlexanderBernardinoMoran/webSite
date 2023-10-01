d5650dc34b116da13aaace3b06f723bb7eee81c7
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-09-30 21:01:01 -0600

    Creando el la seccion del home y sobre mí

--- resources/views/livewire/show-portfolio.blade.php
+++ resources/views/livewire/show-portfolio.blade.php
@@ -1,1 +1,31 @@
+<!-- ---Home Section Desing--- -->
+<section class="home" id="home">
+    <div class="home-text">
+        <div class="slide">
+            <span class="one">Hello</span>
+            <span class="two">I'm</span>
+        </div>
+        <h1>Edwin Alexander</h1>
+        <h3>FullStack <span>Developer.</span></h3>
+        <p>FullStack Developer with 2+ years experiencie that keep customers <br> comming back for about services makes best effort.</p>
+        <div class="button">
+            <a href="" class="btn">Dowload CV</a>
+        </div>
+    </div>
+</section>
 
+<!-- ---About Section Design--- -->
+<section class="about" id="about">
+    <div class="about-img">
+        <img src="./img/edwin_edit.png" alt="about" width="400px" height="570px">
+    </div>
+
+    <div class="about-text">
+        <h2>About <span>Me</span></h2>
+        <h4>Creative Development Web</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odit voluptatibus tempore facere maxime molestiae dolore quo consequatur similique nihil minus sit, ex nulla quae, provident aut magni impedit unde eveniet error incidunt quos vitae expedita magnam. Voluptates ipsum vel voluptate quibusdam omnis. Sit totam assumenda est, exercitationem nam laboriosam ullam enim nemo repellat tenetur iste hic eum.
+        </p>
+        <a href="#" class="btn">More About</a>
+    </div>
+</section>
