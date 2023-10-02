82a332ab29d89a4b15244c25fd9d21775ebf55bb
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-10-02 16:16:28 -0600

    Cambiando url de archivo app

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     */
 
     // 'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'https://edwinalexander.up.railway.app'),
+    'url' => env('APP_URL', 'https://website-production-718a.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
