23249456696c79d96a1ac5ce23f86fe9ceea474f
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-10-18 15:12:11 -0600

    adding boolean values to work with https

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -7,8 +7,8 @@
     <title>Téc. Edwin Alexander</title>
 
     {{-- Estilos CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('icons/css/boxicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('icons/css/boxicons.min.css', true) }}">
 </head>
 <body>
 
@@ -23,7 +23,7 @@
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> <!-- Importa Axios -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('icons/dist/boxicons.js') }}"></script>
+    <script src="{{ asset('js/app.js', true) }}"></script>
+    <script src="{{ asset('icons/dist/boxicons.js', true) }}"></script>
 </body>
 </html>
